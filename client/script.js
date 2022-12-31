888b6f3fcc9cc283c53d56cfeb3cccb1c007fdb7
Tommy-Burns <58440453+Tommy-Burns@users.noreply.github.com>
2022-12-31 01:51:42 +0000

    backend url added

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 	loader(messageDiv);
 
 	// data from server
-	const response = await fetch("http://localhost:5000", {
+	const response = await fetch("https://gpt-clone.onrender.com", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
